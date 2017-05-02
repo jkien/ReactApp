8b2108c307a8e9431de27da4a343abf252e5fc41
Jason Kien <kien.jason@gmail.com>
2017-05-02 00:47:01 -0400

    Add get friends list

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -17,8 +17,8 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1>JASON RULES</h1>
           <h2>Welcome to React</h2>
-          <Login />
         </div>
+        <Login />
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
--- react-app/src/Login.js
+++ react-app/src/Login.js
@@ -1,6 +1,7 @@
 import React, { PropTypes, Component } from 'react';
 import ReactDOM from 'react-dom';
 import FacebookLogin from 'react-facebook-login';
+import graph from 'fb-react-sdk';
 
 import { connect } from 'react-redux'
 
@@ -23,6 +24,15 @@ export class Login extends Component {
     this.setState( {fbUser: response});
     console.log("state:");
     console.log(this.state);
+    
+    graph.setAccessToken(this.state.fbUser.accessToken);
+    //graph.get(this.state.fbUser.id + '/friendlists', function(err, res) {
+    //this is another way to call graph api, already have this info in facebook login call response
+    graph.get('/me/friends', function(err, res) {
+      console.log('getting friends list with graph api:');
+      console.log(res); // { id: '4', name: 'Mark Zuckerberg'... } 
+      console.log(err);
+    });
   }
 
   render = () => (
@@ -30,15 +40,22 @@ export class Login extends Component {
       <FacebookLogin
         appId="1126658334110806"
         autoLoad={true}
-        fields="name,email,picture"
+        fields="name,email,picture,friends"
+        scope="public_profile,user_friends,user_actions.books"
         callback={this.responseFacebook} />
         <br/><br/>
       <div>
         {!!(this.state.fbUser) &&
           <div>
-            {this.state.fbUser.name}
+            <div>{this.state.fbUser.name}</div>
             {!!(this.state.fbUser.picture) &&
               <img src={this.state.fbUser.picture.data.url} />
+            }
+            {!!(this.state.fbUser.friends) &&
+              <div>
+                <div>You have {this.state.fbUser.friends.summary.total_count} friends.</div>
+                <div>{this.state.fbUser.friends.data.length} are using this app.</div>
+              </div>
             }
           </div>
         }
